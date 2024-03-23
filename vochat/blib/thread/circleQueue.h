32402b61f546e6b709564d076940e1eb48f8e36b
Bulat Sadykov <bulatsad111199@gmail.com>
2024-03-23 17:08:25 +0300

    rewrited cirle queue to lockless style

--- vochat/blib/thread/circleQueue.h
+++ vochat/blib/thread/circleQueue.h
@@ -1,3 +1,7 @@
+/*
+Single producer - single consumer thread safe queue
+*/
+
 #pragma once
 
 #include <blib/align.h>
@@ -5,40 +9,37 @@
 
 #include <memory>
 #include <mutex>
+#include <atomic>
 
 namespace blib
 {
     template<class _Ty, class _Alloc = std::allocator<_Ty> >
-    class CircleQueue
+    class __blib_cache_aligned LocklessProducerConcumerCircleQueue
     {
     private:
-        __blib_cache_aligned _Alloc allocator;
         __blib_cache_aligned _Ty* data;
-        __blib_cache_aligned size_t reader;
-        __blib_cache_aligned size_t writer;
-        __blib_cache_aligned size_t size;
+        __blib_cache_aligned std::atomic<size_t> reader;
+        __blib_cache_aligned std::atomic<size_t> writer;
         __blib_cache_aligned size_t capacity;
-        __blib_cache_aligned std::mutex m;
+        _Alloc allocator;
     public:
         template<class ...Args>
-        CircleQueue(size_t capacity, Args&& ... args);
-        ~CircleQueue();
+        LocklessProducerConcumerCircleQueue(size_t capacity, Args&& ... args);
+        ~LocklessProducerConcumerCircleQueue();
         bool push(const _Ty& d);
+        bool push(_Ty&& d);
         bool pop(_Ty& res);
     };
 }
 
-
-
 template<class _Ty, class _Alloc>
 template<class ...Args>
-__blib_inline blib::CircleQueue<_Ty, _Alloc>::CircleQueue(size_t capacity, Args&& ... args)
+__blib_inline blib::LocklessProducerConcumerCircleQueue<_Ty, _Alloc>::LocklessProducerConcumerCircleQueue(size_t capacity, Args&& ... args)
 {
     this->data = this->allocator.allocate(capacity);
-    this->capacity = capacity;
-    this->reader = 0;
-    this->writer = 0;
-    this->size = 0;
+    this->capacity = capacity; 
+    this->writer.store(0, std::memory_order::memory_order_release);
+    this->reader.store(0, std::memory_order::memory_order_release);
 
     for (size_t i = 0; i < this->capacity; i++)
         this->allocator.construct(&(this->data[i]), std::forward<Args>(args)...);
@@ -46,7 +47,7 @@ __blib_inline blib::CircleQueue<_Ty, _Alloc>::CircleQueue(size_t capacity, Args&
 }
 
 template<class _Ty, class _Alloc>
-__blib_inline blib::CircleQueue<_Ty, _Alloc> ::~CircleQueue()
+__blib_inline blib::LocklessProducerConcumerCircleQueue<_Ty, _Alloc> ::~LocklessProducerConcumerCircleQueue()
 {
     for (size_t i = 0; i < this->capacity; i++)
         this->allocator.destroy(&(this->data[i]));
@@ -54,36 +55,67 @@ __blib_inline blib::CircleQueue<_Ty, _Alloc> ::~CircleQueue()
 }
 
 template<class _Ty, class _Alloc>
-__blib_inline bool blib::CircleQueue<_Ty, _Alloc>::push(const _Ty& d)
+__blib_inline bool blib::LocklessProducerConcumerCircleQueue<_Ty, _Alloc>::push(const _Ty& d)
 {
-    if (this->size == this->capacity)
-    {
-        return false;
-    }
-    MutexLocker<std::mutex> locker(&m);
-    this->data[this->writer] = d;
-    this->size++;
-    if (this->writer + 1 >= this->capacity)
-        this->writer = 0;
+    const size_t w = this->writer.load(std::memory_order::memory_order_relaxed);
+    const size_t r = this->reader.load(std::memory_order::memory_order_acquire);
+    bool writable;
+
+    if (r > w)
+        writable = w + 1 != r;
+    else if (r < w)
+        writable = w + 1 == this->capacity ? r != 0 : true;
     else
-        this->writer++;
+        writable = true;
+
+    if (writable == 0)
+        return false;
+
+    this->data[w] = d;
+    if (w + 1 == this->capacity)
+        this->writer.store(0, std::memory_order::memory_order_release);
+    else
+        this->writer.store(w + 1, std::memory_order::memory_order_release);
     return true;
 }
 
 template<class _Ty, class _Alloc>
-__blib_inline bool blib::CircleQueue<_Ty, _Alloc>::pop(_Ty& res)
+__blib_inline bool blib::LocklessProducerConcumerCircleQueue<_Ty, _Alloc>::push(_Ty&& d)
 {
-    if (this->size > 0)
-    {
+    const size_t w = this->writer.load(std::memory_order::memory_order_relaxed);
+    const size_t r = this->reader.load(std::memory_order::memory_order_acquire);
+    bool writable;
 
-        MutexLocker<std::mutex> locker(&m);
-        this->size--;
-        res = std::move(this->data[this->reader]);
-        if (this->reader + 1 >= this->capacity)
-            this->reader = 0;
-        else
-            this->reader++;
-        return true;
-    }
-    return false;
+    if (r > w)
+        writable = w + 1 != r;
+    else if (r < w)
+        writable = w + 1 == this->capacity ? r != 0 : true;
+    else
+        writable = true;
+
+    if (writable == 0)
+        return false;
+
+    this->data[w] = std::move(d);
+    if (w + 1 == this->capacity)
+        this->writer.store(0, std::memory_order::memory_order_release);
+    else
+        this->writer.store(w + 1, std::memory_order::memory_order_release);
+    return true;
+}
+
+template<class _Ty, class _Alloc>
+__blib_inline bool blib::LocklessProducerConcumerCircleQueue<_Ty, _Alloc>::pop(_Ty& res)
+{
+    const std::size_t w = this->writer.load(std::memory_order::memory_order_relaxed);
+    const std::size_t r = this->reader.load(std::memory_order::memory_order_acquire);
+    if (w == r)
+        return false;
+
+    res = std::move(this->data[this->reader]);
+    if (r + 1 == this->capacity)
+        this->reader.store(0, std::memory_order::memory_order_release);
+    else
+        this->reader.store(r + 1, std::memory_order::memory_order_release);
+    return true;
 }
