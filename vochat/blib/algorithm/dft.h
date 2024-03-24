5b0c740b2df22cc1d6148fc4e84c5539a4688a02
bulatsad111199@gmail.com <T-800SADsad>
2024-03-25 00:47:15 +0300

    tempCommit added expDft added expFdft

---
+++ vochat/blib/algorithm/dft.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+constexpr double PI = M_PI;
+constexpr double _2PI = PI * 2;
+
+namespace blib
+{
+    template<class T, class Allocator>
+    struct Spectre
+    {
+        std::vector<T> a;
+        std::vector<T> b;
+        size_t size;
+
+        Allocator allocator;
+
+        Spectre(size_t _size);
+        ~Spectre();
+    };
+
+    template<class InT, class SpectreT = double, class Allocator = std::allocator<SpectreT> >
+    Spectre<SpectreT, Allocator> fourierTransform(const void* pdata, size_t size);
+
+    template<class Out, class T, class Allocator = std::allocator<T> >
+    std::vector<Out> inverseFourierTransform(const Spectre<T, Allocator>* spectre);
+}
+
+template<class T, class Allocator>
+inline blib::Spectre<T, Allocator>::Spectre(size_t _size)
+{
+    //this->size = _size;
+    //
+    //this->a = this->allocator.allocate(this->size);
+    //this->b = this->allocator.allocate(this->size);
+    //
+    //memset(this->a, 0, this->size);
+    //memset(this->b, 0, this->size);
+
+    this->a.resize(_size);
+    this->b.resize(_size);
+    this->size = _size;
+}
+
+template<class T, class Allocator>
+inline blib::Spectre<T, Allocator>::~Spectre()
+{
+    //this->allocator.deallocate(this->a, this->size);
+    //this->allocator.deallocate(this->b, this->size);
+}
+
+template<class InT, class SpectreT, class Allocator>
+blib::Spectre<SpectreT, Allocator> blib::fourierTransform(const void* pdata, size_t a_size)
+{
+    using Spectre_t = Spectre<SpectreT, Allocator>;
+    const InT* wave = reinterpret_cast<const InT*>(pdata);
+    size_t size = a_size; // sizeof(InT);
+    Spectre_t spectre(size);
+
+    for (size_t k = 0; k < size; ++k)
+    {
+        for (size_t n = 0; n < size ; ++n)
+        {
+            spectre.b[k] += wave[n] * sin(k * (static_cast<SpectreT>(_2PI) / size) * n);
+            spectre.a[k] += wave[n] * cos(k * (static_cast<SpectreT>(_2PI) / size) * n);
+        }
+
+        spectre.b[k] = spectre.b[k] / size;
+        spectre.a[k] = spectre.a[k] / size;
+    }
+
+    return spectre;
+}
+
+template<class Out, class SpectreT, class Allocator>
+std::vector<Out> blib::inverseFourierTransform(const blib::Spectre<SpectreT, Allocator>* spectre)
+{
+    std::vector<Out>res;
+    res.resize(spectre->size,0);
+
+    for (size_t k = 0; k < spectre->size; ++k)
+    {
+        SpectreT currentFt = 0;
+        for (size_t n = 1; n < spectre->size; ++n)
+        {
+            currentFt +=
+                spectre->a[n] * cos(k * (static_cast<SpectreT>(_2PI) / spectre->size) * n) +
+                spectre->b[n] * sin(k * (static_cast<SpectreT>(_2PI) / spectre->size) * n);
+        }
+        res[k] = static_cast<Out>(currentFt) + spectre->a[0];
+    }
+
+    return res;
+}
--- vochat/core/src/main.cpp
+++ vochat/core/src/main.cpp
@@ -4,42 +4,173 @@
 #include <Windows.h>
 
 #include <iostream>
+#include<blib/algorithm/dtfExp.h>
+#include<blib/algorithm/dft.h>
 
+//int main()
+//{
+//    {
+//        std::cout << sizeof(blib::LocklessProducerConcumerCircleQueue<int>) << std::endl;
+//        blib::SoundRecorder recorder;
+//        recorder.setFormat(blib::SoundFormat(1, 44100, 16));
+//        bool isopened = recorder.open();
+//
+//        if (isopened)
+//        {
+//            std::cout << "opened!" << std::endl;
+//        }
+//
+//        recorder.start();
+//
+//        Sleep(1000);
+//
+//        recorder.stop();
+//
+//        blib::SoundPlayer player;
+//        player.setFormat(blib::SoundFormat(1, 44100, 16));
+//        isopened = player.open();
+//
+//        clock_t start = clock();
+//
+//        auto spectre = blib::fourierTransform<short>(recorder.buffers[0].getData(), 
+//            recorder.buffers[0].getSize());
+//
+//        clock_t end = clock() - start;
+//
+//        std::cout << "ft : " << end << std::endl;
+//
+//        auto data = blib::inverseFourierTransform<short>(&spectre);
+//
+//        clock_t iftend = clock() - end;
+//
+//        std::cout << "ift : " << end << std::endl;
+//
+//        blib::SoundBuffer iftbuffer(blib::SoundFormat(1, 44100, 16),data.size() / 2,data.size() / 2,data.data());
+//
+//        blib::SoundBuffers sbs;
+//        sbs.emplace_back(std::move(iftbuffer));
+//
+//        player.setData(std::move(sbs));
+//
+//        if (isopened)
+//        {
+//            std::cout << "opened!" << std::endl;
+//            player.play();
+//
+//            Sleep(5000);
+//        }
+//
+//        player.close();
+//        recorder.close();
+//
+//    }
+//    return 0;
+//
+//}
+
+typedef std::complex<double> base;
+
+void fft(std::vector<base>& a, bool invert) {
+    int n = (int)a.size();
+    if (n == 1)  
+        return;
+
+    std::vector<base> a0(n / 2), a1(n / 2);
+    for (int i = 0, j = 0; i < n; i += 2, ++j) {
+        a0[j] = a[i];
+        a1[j] = a[i + 1];
+    }
+    fft(a0, invert);
+    fft(a1, invert);
+
+    double ang = 2 * PI / n * (invert ? -1 : 1);
+    base w(1), wn(cos(ang), sin(ang));
+    for (int i = 0; i < n / 2; ++i) {
+        a[i] = a0[i] + w * a1[i];
+        a[i + n / 2] = a0[i] - w * a1[i];
+        if (invert)
+            a[i] /= 2, a[i + n / 2] /= 2;
+        w *= wn;
+    }
+}
 
 int main()
 {
-    std::cout << sizeof(blib::LocklessProducerConcumerCircleQueue<int>) << std::endl;
-    blib::SoundRecorder recorder;
-    recorder.setFormat(blib::SoundFormat(1, 44100, 16));
-    bool isopened = recorder.open();
+    std::vector<double>wave({ 
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,
+        });
 
-    if (isopened)
-    {
-        std::cout << "opened!" << std::endl;
-    }
-
-    recorder.start();
+    std::vector<short>simpleWave({ 4,3,0,-3,-4,-3,0,3 });
     
-    Sleep(3000);
+    std::vector<std::complex<double> >sw;
+    for (auto& s : wave)
+        sw.push_back(std::complex<double>(s, 0));
+
+    sw.resize(512);
+    wave.resize(512);
+    std::vector<std::complex<double> >sw1(sw);
+
+
+    clock_t fftstart = clock();
+    fft(sw, false);
+    clock_t fftend = clock();
+    clock_t ffttime = fftend - fftstart;
+    fft(sw, true);
+    clock_t ifftt = clock() - fftend;
+
+    //auto c = blib::expFastFourierTransform(sw1);
+    //auto wave1 = blib::expInverseFourierTransform(c);
     
-    recorder.stop();
+    //auto c = blib::fourierTransform<short>(simpleWave.data(), simpleWave.size());
+    //auto wave1 = blib::inverseFourierTransform<short>(&c);
+    
+    clock_t ftstart = clock();
+    auto spectre = blib::expFastFourierTransform(sw1);
+    clock_t ftend = clock();
+    clock_t fttime = ftend - ftstart;
+    auto wave1 = blib::expFastInverseFourierTransform(spectre);
+    clock_t iftt = clock() - ftend;
 
-    blib::SoundPlayer player;
-    player.setFormat(blib::SoundFormat(1, 44100, 16));
-    isopened = player.open();
+    //clock_t ftstart = clock();
+    //auto spectre = blib::fourierTransform<double, double>(wave.data(), wave.size());
+    //clock_t ftend = clock();
+    //clock_t fttime = ftend - ftstart;
+    //auto wave1 = blib::inverseFourierTransform<double, double>(&spectre);
+    //clock_t iftt = clock() - ftend;
 
-    player.setData(std::move(recorder.buffers));
 
-    if (isopened)
-    {
-        std::cout << "opened!" << std::endl;
-        player.play();
-
-        Sleep(5000);
-    }
-
-    player.close();
-    recorder.close();
 
     return 0;
 }
