2f567af95081c4366e4788643972e9b3b6b7b5e6
Bulat Sadykov <bulatsad111199@gmail.com>
2024-07-07 06:20:04 +0300

    first prod test

--- vochat/core/src/main.cpp
+++ vochat/core/src/main.cpp
@@ -8,6 +8,8 @@
 #include<blib/algorithm/dft.h>
 
 #include<blib/network/udpSocket.h>
+#include<blib/network/tcpSocket.h>
+#include<blib/network/tcpListener.h>
 #include <blib/core/string.h>
 
 //int main()
@@ -176,49 +178,115 @@ void fft(std::vector<base>& a, bool invert) {
 //    return 0;
 //}
 
-int main()
-{
-    blib::network::InitBlibSocket();
+//int main()
+//{
+//    blib::network::InitBlibSocket();
+//
+//    blib::network::UdpSocket udps(blib::network::AddressType::IPv4);
+//
+//    char mode;
+//    std::cin >> mode;
+//
+//    char data[5] = { 1,2,3,4,5 };
+//
+//    if (mode == 'c')
+//    {
+//        bool ok = false;
+//        auto addr = blib::network::Address::fromIPv4("192.168.1.51", &ok);
+//        addr.setPort(8080);
+//        udps.send(addr, data, 5);
+//    }
+//
+//    if (mode == 's')
+//    {
+//        char rd[5] = {0,0,0,0,0};
+//        int rs = 5;
+//        bool ok = false;
+//        auto addr = blib::network::Address::fromIPv4("0.0.0.0", &ok);
+//        addr.setPort(8080);
+//        udps.bind(addr);
+//
+//        blib::network::Address sender;
+//
+//        udps.recv(sender, rd, rs);
+//
+//        std::cout
+//            << (int)rd[0] << " " 
+//            << (int)rd[1] << " " 
+//            << (int)rd[2] << " " 
+//            << (int)rd[3] << " "
+//            << (int)rd[4] << " ";
+//    }
+//
+//
+//    return 0;
+//}
 
-    blib::network::UdpSocket udps(blib::network::AddressType::IPv4);
-
-    char mode;
-    std::cin >> mode;
-
-    char data[5] = { 1,2,3,4,5 };
-
-    if (mode == 'c')
-    {
-        bool ok = false;
-        auto addr = blib::network::Address::fromIPv4("192.168.1.51", &ok);
-        addr.setPort(8080);
-        udps.send(addr, data, 5);
-    }
-
-    if (mode == 's')
-    {
-        char rd[5] = {0,0,0,0,0};
-        int rs = 5;
-        bool ok = false;
-        auto addr = blib::network::Address::fromIPv4("0.0.0.0", &ok);
-        addr.setPort(8080);
-        udps.bind(addr);
-
-        blib::network::Address sender;
-
-        udps.recv(sender, rd, rs);
-
-        std::cout
-            << (int)rd[0] << " " 
-            << (int)rd[1] << " " 
-            << (int)rd[2] << " " 
-            << (int)rd[3] << " "
-            << (int)rd[4] << " ";
-    }
-
-
-    return 0;
-}
+//int main()
+//{
+//    blib::network::InitBlibSocket();
+//
+//    char mode;
+//    std::cin >> mode;
+//
+//    char data[5] = { 1,2,3,4,5 };
+//
+//    if (mode == 'c')
+//    {
+//        blib::network::TcpSocket connector(blib::network::AddressType::IPv4);
+//        //connector.setBlocking(false);
+//        bool ok = false;
+//        auto addr = blib::network::Address::fromIPv4("192.168.1.51", &ok); //
+//        addr.setPort(8080);
+//        while (connector.connect(addr) != blib::network::SocketStatus::OK);
+//        while (connector.send(data, 5) != blib::network::SocketStatus::OK);
+//
+//        char rd[5] = { 0,0,0,0,0 };
+//        int rs = 5;
+//
+//        connector.setBlocking(false);
+//        while (connector.recv(rd, rs) != blib::network::SocketStatus::OK);
+//
+//        std::cout
+//            << (int)rd[0] << " "
+//            << (int)rd[1] << " "
+//            << (int)rd[2] << " "
+//            << (int)rd[3] << " "
+//            << (int)rd[4] << " ";
+//    }
+//
+//    if (mode == 's')
+//    {
+//        blib::network::TcpListener listner(blib::network::AddressType::IPv4);
+//        blib::network::TcpSocket acc;
+//        //listner.setBlocking(false);
+//
+//        char rd[5] = {0,0,0,0,0};
+//        int rs = 5;
+//        bool ok = false;
+//        auto addr = blib::network::Address::fromIPv4("0.0.0.0", &ok);
+//        addr.setPort(8080);
+//        listner.bind(addr);
+//        while (listner.listen()    != blib::network::SocketStatus::OK);
+//        while (listner.accept(acc) != blib::network::SocketStatus::OK);
+//        
+//        acc.setBlocking(false);
+//        while (acc.recv(rd, rs) != blib::network::SocketStatus::OK);
+//
+//        std::cout
+//            << (int)rd[0] << " " 
+//            << (int)rd[1] << " " 
+//            << (int)rd[2] << " " 
+//            << (int)rd[3] << " "
+//            << (int)rd[4] << " ";
+//
+//        while (acc.send(data, 5) != blib::network::SocketStatus::OK);
+//
+//    }
+//
+//
+//    return 0;
+//}
 
 //int main()
 //{
@@ -250,5 +318,78 @@ int main()
 //    recorder.stop();
 //
 //    return 0;
-//
 //}
+
+int main()
+{
+    blib::network::InitBlibSocket();
+    std::string connectionIp;
+    std::string connectinPort;
+    std::string mode;
+
+    std::cout << "mode s/c" << std::endl;
+    std::cin >> mode;
+
+    std::cout << "connection ip" << std::endl;
+    std::cin >> connectionIp;
+    std::cout << "connection port" << std::endl;
+    std::cin >> connectinPort;
+
+    blib::network::TcpSocket socket(blib::network::AddressType::IPv4);
+
+    if (mode == "c")
+    {
+        bool ok = false;
+        auto addr = blib::network::Address::fromIPv4(connectionIp.c_str(), &ok); //
+        addr.setPort(atoi(connectinPort.c_str()));
+        while (socket.connect(addr) != blib::network::SocketStatus::OK);
+        std::cout << "connected" << std::endl;
+    }
+
+    if (mode == "s")
+    {
+        blib::network::TcpListener listner(blib::network::AddressType::IPv4);
+
+        bool ok = false;
+        auto addr = blib::network::Address::fromIPv4(connectionIp.c_str(), &ok);
+        addr.setPort(atoi(connectinPort.c_str()));
+        listner.bind(addr);
+        while (listner.listen() != blib::network::SocketStatus::OK);
+        while (listner.accept(socket) != blib::network::SocketStatus::OK);
+        std::cout << "accepted" << std::endl;
+    }
+
+    socket.setBlocking(false);
+
+    blib::RealTimeSoundRecorder recorder;
+    recorder.setFormat(blib::SoundFormat(1, 44100, 16));
+    recorder.setBufferInfo(10, 50);
+
+    blib::RealTimeSoundPlayer player;
+    player.setFormat(blib::SoundFormat(1, 44100, 16));
+    player.setBufferInfo(10, 50);
+
+    recorder.open();
+    player.open();
+
+    recorder.start();
+
+    while (1)
+    {
+        auto recordedFrame = recorder.acquireBuffer();
+        socket.send(recordedFrame.sndFrame->getData(), recordedFrame.sndFrame->getSize());
+        recorder.releaseBuffer(std::move(recordedFrame));
+
+
+        auto playingFrame = player.acquireBuffer();
+        memset(playingFrame.sndFrame->getData(), playingFrame.sndFrame->getSize(), 0);
+        
+        int recvsize = playingFrame.sndFrame->getSize();
+        socket.recv(playingFrame.sndFrame->getData(), recvsize);
+        player.releaseBuffer(std::move(playingFrame));
+    }
+
+    recorder.stop();
+
+    return 0;
+}//192.168.1.51
