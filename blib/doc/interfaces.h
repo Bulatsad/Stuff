2a9fe05e3dd14537213acf736c3e864d9c8aa60a
bulatsad111199@gmail.com <T-800SADsad>
2026-06-24 21:50:46 +0300

    cleanup: remove old root-level paths after restructure

--- blib/doc/interfaces.h
+++
@@ -1,6 +0,0 @@
-class Transormable
-{
-public:
-    vector3f getPosition();
-    void setPosition(const vector3f& pos);
-};
--- blib/utilmacro.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define _In
-#define _Out
-#define __blib_override override
